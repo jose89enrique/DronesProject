5cb52621283dd9efd6d67174334dc5bcd7d82430
JoseEnrique <jose89enrique@gmail.com>
2023-06-18 22:16:49 -0500

    Services included

--- DroneDB.cs
+++ DroneDB.cs
@@ -58,34 +58,128 @@ public class DroneDB
     //Registering a drone
     public static object registerDrone(RegisterDrone newDrone)
     {    
-        
-        return null;     
+        Drone? result;
+        if(newDrone.SerialNumber!=null && newDrone.SerialNumber.Length>1000)
+            return new {error="Wrong serial number; the limit is 1000 characters"};  
+        else
+        {
+            int count = (from dr in _drones where dr.SerialNumber==newDrone.SerialNumber select dr).Count();
+            if(count>0)
+                return new {error = "The serial number is wrong. Exist another registered drone with this number."};
+        }
+
+        if(newDrone.weight<0 || newDrone.weight>500)
+            return new {error="The weight is over the limit! It has to be positive and under 500gr"};      
+        else
+        {
+            result=new Drone(){SerialNumber= newDrone.SerialNumber, model=newDrone.model, weight=newDrone.weight, state=State.IDLE, batteryCapacity=100, load=new List<Medication>()};
+            _drones.Add(result);
+            return result;  
+        } 
     }
 
     //Loading a drone with medication items
     public static object loadMedication(LoadDrone input)
     {
-        
-        return null;
+        string chars= "abcdefghijklmnopqrstuvwxyz";
+        string numbers="0123456789";
+        Drone? droneM= _drones.SingleOrDefault(dr => dr.SerialNumber == input.SerialNumber);
+        if(droneM != default(Drone))
+        {            
+            int currentLoad= (from dr in droneM.load select dr.weight).Sum();
+            droneM.state=State.LOADING;
+            if(input.load!=null)
+            {
+                foreach (Medication item in input.load)
+                {
+                    //Only letters and numbers, -, _ in the name
+                    if(item.name!=null)
+                    {
+                        for (int i=0; i<item.name.Length;i++)
+                        {
+                            if(!(chars+chars.ToUpper()+numbers+"-_").Contains(item.name[i]))
+                                return new {error= string.Format("The name {0} is wrong for a medication. Just be allowed letters, numbers, - and underscore.", item.name)};                            
+                        }
+                    }
+                    else return new {error= string.Format("The name {0} is wrong for a medication. Just be allowed letters, numbers, - and underscore.", item.name)};
+                    
+                    //Code: allowed only upper case letters, underscore and numbers
+                    if(item.code!=null)
+                    {
+                        for (int i=0; i<item.code.Length;i++)
+                        {
+                            if(!(chars.ToUpper()+numbers+"_").Contains(item.code[i]))
+                                return new {error= string.Format("The code {0} is wrong for a medication. Just be allowed letters, numbers and underscore.", item.code)};                            
+                        }
+                    }
+                    else return new {error= string.Format("The code {0} is wrong for a medication. Just be allowed letters, numbers and underscore.", item.code)};
+                    
+                    if(currentLoad+item.weight>droneM.weight)
+                        return new {error= "The Medications exceed load limits."};
+                    else
+                    {
+                        currentLoad+=item.weight;
+                        if(droneM.load==null)
+                            droneM.load=new List<Medication>(){item};
+                        else
+                            droneM.load.Add(item);
+                    }
+                }
+            }
+            droneM.state=State.LOADED;
+
+            if(droneM.batteryCapacity<25)
+                return new {warning= "The battery level is below 25 percent"};
+            else
+            {
+                return droneM;
+            }
+        }
+        else
+        {
+            return new {error= String.Format("The drone {0} is not registered", input.SerialNumber)};
+        }
     }
 
     //Checking loaded medication items for a given drone
     public static object checkLoadDrone(string droneSN)
     {
-        return null;
+       if(droneSN == default(string))
+            return new {error= "Empty serial number!"};
+        else
+        {
+            Drone? droneM= _drones.SingleOrDefault(dr => dr.SerialNumber == droneSN);
+           // List<Drone> result= (from dr in _drones where dr.SerialNumber==droneSN select dr).ToList();
+            if(droneM == default(Drone))
+                return new {error=String.Format("Don't exist any drone with the serial number {0}", droneSN)};
+            else
+            {                         
+                if(droneM.load!=null)                       
+                    return droneM.load;
+                else
+                    return new List<Drone>(); 
+            }
+        }
              
     }
 
     //Checking avaible drones for loading
     public static List<Drone> checkDronesReady()
     {
-        return null;
+        List<Drone> result=(from dr in _drones where dr.state==State.IDLE select dr).ToList();
+
+        return result;
     }
 
     //Check drone battery level for a given drone 
     public static object checkBatteryDrone(string serial)
     {
-        return null;
+         Drone? droneM= _drones.SingleOrDefault(dr => dr.SerialNumber == serial);
+        //List<Drone> result=(from dr in _drones where dr.state==State.IDLE select dr).ToList();
+        if(droneM == default(Drone))
+            return new {error=String.Format("Don't exist any drone with the serial number {0}", serial)};
+        else            
+            return droneM.batteryCapacity;
     }
 
     
--- Program.cs
+++ Program.cs
@@ -18,5 +18,14 @@ app.UseSwaggerUI(c =>
 });
 
 
+app.MapPost("/drones", (RegisterDrone newDrone)=> DroneDB.registerDrone(newDrone));
+
+app.MapPost("/drones/load", (LoadDrone loadM)=> DroneDB.loadMedication(loadM));
+
+app.MapGet("/drones/medication/{serial}", (string serial)=>DroneDB.checkLoadDrone(serial));
+
+app.MapGet("/drones/ready", ()=>DroneDB.checkDronesReady());
+
+app.MapGet("/drones/battery/{serial}", (string serial)=>DroneDB.checkBatteryDrone(serial));
 
 app.Run();
